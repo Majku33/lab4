ce5a3d7cb0824211e6bd4e118dbff2c39f8bc745
Majku33 <118014414+Majku33@users.noreply.github.com>
2023-06-25 10:49:51 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,1 +1,55 @@
+#!/bin/bash 
 
+ 
+
+show_help() { 
+
+    echo "Użycie: skrypt.sh [OPCJE]" 
+
+    echo "Opcje:" 
+
+    echo "  --help        Wyświetla pomoc" 
+
+    echo "  --data        Używa dzisiejszej daty" 
+
+    echo "  --logs100     Tworzy 100 plików z logami" 
+
+    echo "  --logs30 [N]  Tworzy N plików z logami" 
+
+    echo "                (domyślnie N = 30)" 
+
+} 
+
+ 
+
+create_log_file() { 
+
+    local num_plikow=$1 
+
+    for ((i=1; i<=num_plikow; i++)); do 
+
+        nazwa_pliku="log${i}.txt" 
+
+        dzisiaj=$(date +"%Y-%m-%d") 
+
+        echo "Nazwa pliku: $nazwa_pliku" > "$nazwa_pliku" 
+
+        echo "Nazwa skryptu: skrypt.sh" >> "$nazwa_pliku" 
+
+        echo "Data utworzenia: $dzisiaj" >> "$nazwa_pliku" 
+
+    done 
+
+} 
+
+ 
+
+if [[ $1 == "--help" ]]; then 
+
+    show_help 
+
+else 
+
+    echo "Nieznana opcja. Użyj --help, aby wyświetlić pomoc." 
+
+fi 
